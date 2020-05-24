2278b12c108d8d016e200b98694000ac8c4feb94
MrinmoiHossain <mokit.aust@gmail.com>
2020-05-24 00:51:19 +0000

    kubectl run script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="mrinmoi/api"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run myapp\
+	--image=$dockerpath\
+       	--port=80 --labels app=myapp
 
 # Step 3:
 # List kubernetes pods
+kubectl get pod
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward myapp 8000:80
